7a6b546cefe743d3eaff44d5672123061d5cba1f
Diego Tenório <38768837+diegoptenorio@users.noreply.github.com>
2023-12-28 19:30:26 -0300

    task: Button component teste

---
+++ src/components/button/button.spec.tsx
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+import { Button } from '@components'
+
+describe('Button', () => {
+  test('Should validate button value', () => {
+    const Wrapper = render(
+        <Button value='Add feature' />
+    )
+    const text = Wrapper.queryByText('Add feature')
+    expect(text).toBeInTheDocument()
+  });
+});
