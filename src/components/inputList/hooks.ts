eebf986b1e023417b9cb6d908a2cff1c22e63cb4
Diego Tenório <38768837+diegoptenorio@users.noreply.github.com>
2024-01-04 08:22:18 -0300

    task: Tear business constants apart

--- src/components/inputList/hooks.ts
+++ src/components/inputList/hooks.ts
@@ -1,12 +1,18 @@
 import { useState } from 'react';
 import { IInputList } from './types';
 
+const BUSINESS = {
+    DEFAULT_LABEL: 'What is the name of the task?',
+    PLACEHOLDER_BASE: 'Ex: Task',
+    MAX_INPUT_QUANTITY: 2,
+};
+
 export const useInputList = () => {
     const [inputList, setInputList] = useState([
         {
             id: 0,
-            label: 'What is the name of the task?',
-            placeholder: 'Ex: Task 1',
+            label: BUSINESS.DEFAULT_LABEL,
+            placeholder: `${BUSINESS.PLACEHOLDER_BASE} 1`,
             remove: false,
             value: '',
         },
@@ -14,7 +20,7 @@ export const useInputList = () => {
     const [disabledAddButton, setDisabledAddButton] = useState(false);
 
     const handleTeste = () => {
-        if (inputList.length < 3) {
+        if (inputList.length <= BUSINESS.MAX_INPUT_QUANTITY) {
             const lastTeste = inputList[inputList.length - 1];
             setInputList((previous: IInputList[]) => {
                 return previous
@@ -23,7 +29,9 @@ export const useInputList = () => {
                           {
                               id: lastTeste.id + 1,
                               label: '',
-                              placeholder: `Ex: Task ${lastTeste.id + 2}`,
+                              placeholder: `${BUSINESS.PLACEHOLDER_BASE} ${
+                                  lastTeste.id + 2
+                              }`,
                               remove: true,
                               value: '',
                           },
@@ -32,13 +40,15 @@ export const useInputList = () => {
                           {
                               id: lastTeste.id + 1,
                               label: '',
-                              placeholder: `Ex: Task ${lastTeste.id + 2}`,
+                              placeholder: `${BUSINESS.PLACEHOLDER_BASE} ${
+                                  lastTeste.id + 2
+                              }`,
                               remove: true,
                               value: '',
                           },
                       ];
             });
-            if (inputList.length === 2) {
+            if (inputList.length === BUSINESS.MAX_INPUT_QUANTITY) {
                 setDisabledAddButton(true);
             }
         }
